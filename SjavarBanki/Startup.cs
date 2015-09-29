7cd5fe511ffe926c58bbdf0bfbc269057fbd7c96
Fridrikv10 <fridrikv10@ru.is>
2015-09-29 13:03:52 +0000

    Prufa

---
+++ SjavarBanki/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SjavarBanki.Startup))]
+namespace SjavarBanki
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
