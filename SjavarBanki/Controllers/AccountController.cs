e8d6e84639f5f0a04d9e1d2772d981542d446a2f
Fridrikv10 <fridrikv10@ru.is>
2015-09-29 13:16:29 +0000

    Messange

--- SjavarBanki/Controllers/AccountController.cs
+++ SjavarBanki/Controllers/AccountController.cs
@@ -21,6 +21,7 @@ namespace SjavarBanki.Controllers
         public AccountController()
         {
 
+
         }
 
         public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
